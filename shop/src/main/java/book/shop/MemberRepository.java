83fc583416c528d4ab7897b61836756de0e663d0
seohee <hecarisweat@gmail.com>
2023-03-13 23:59:22 +0900

    h2 연결 안됨. 해결중

--- shop/src/main/java/book/shop/MemberRepository.java
+++ shop/src/main/java/book/shop/MemberRepository.java
@@ -1,13 +1,12 @@
 package book.shop;
 
+import org.springframework.stereotype.Repository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import org.springframework.stereotype.Repository;
 
 @Repository
 public class MemberRepository {
 
-    // 엔티티를 영구저장하는 환경 이라는 뜻
     // 어플리케이션과 DB 사이에서 객체를 보관하는 가상의 DB 역할
     @PersistenceContext
     private EntityManager em;
